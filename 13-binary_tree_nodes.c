ccbfd61f3a876dc62111b379aa10c555913fa83f
pablosalina25 <casuarioventas@gmail.com>
2023-09-04 08:21:37 -0700

    arreglos espacios betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,24 +2,17 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_nodes - Cuenta los nodos con al menos 1 hijo
-en un árbol binario.
-* @tree: Un puntero al nodo raíz del árbol
- para contar los nodos con hijos.
-* Return: El número de nodos con al menos 1 hijo
-en el árbol, o 0 si el árbol es NULL.
+* binary_tree_nodes - Count nodes with >= 1 child in a binary tree.
+* @tree: Pointer to the root node to count nodes with children.
+*
+* Return: Number of nodes with >= 1 child, or 0 if tree is NULL.
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-size_t left_nodes, right_nodes;
-/* Caso base: Si el árbol es NULL o es un nodo hoja,
-no hay nodos con hijos. */
-if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+size_t left, right;
+if (!tree || (!tree->left && !tree->right))
 return (0);
-/* Recursivamente contamos los nodos con al menos 1 hijo en los subárboles izquierdo y derecho. */
-left_nodes = binary_tree_nodes(tree->left);
-right_nodes = binary_tree_nodes(tree->right);
-/* Retornamos la suma de los nodos con al menos 1 hijo
-en ambos subárboles más 1 (el nodo actual). */
-return (left_nodes + right_nodes + 1);
+left = binary_tree_nodes(tree->left);
+right = binary_tree_nodes(tree->right);
+return (left + right + 1);
 }
